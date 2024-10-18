595a960f8eee4bd9fa16f540f851b2aa3413dd73
Mahid970 <mmahidujjaman@gmail.com>
2024-10-19 01:11:23 +0600

    completed hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,32 @@
+const Hero = () => {
+  return (
+    <div className=" mx-24 ">
+      <div className="hero rounded-2xl bg-[url('/public/banner.jpg')]">
+        <div className="hero-overlay rounded-2xl bg-opacity-30"></div>
+        <div className="hero-content text-white text-center">
+          <div className="w-2/3 flex flex-col py-20 gap-12">
+            <h1 className=" text-5xl font-bold">
+              Discover an exceptional cooking class tailored for you!
+            </h1>
+            <p className="font-semibold text-white">
+              Learn and Master Basic Programming, Data Structures, Algorithm,
+              OOP, Database and solve 500+ coding problems to become an
+              exceptionally well world-class Programmer
+            </p>
+            <div>
+              {" "}
+              <button className="bg-green-500 py-3 px-6 rounded-full mr-12 text-black  font-bold">
+                Explore Now
+              </button>
+              <button className="py-3 px-6 rounded-full hover:bg-green-500 hover:text-black border font-bold">
+                Our Feedback
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
